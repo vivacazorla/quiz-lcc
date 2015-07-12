11fc244b0ef62e572294d836d51ee791dece6252
Luis <luis_cano@telefonica.net>
2015-07-12 21:59:09 +0200

    version 4.1, Busquedas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -20,7 +20,8 @@ exports.load = function(req, res, next, quizId) {
 
 // GET /quizes
 exports.index = function(req, res) {
-   models.Quiz.findAll().then(function(quizes) {
+     var search = req.query.search?"%" + req.query.search.replace(/ /g,'%') + "%":"%";
+     models.Quiz.findAll({where:["pregunta LIKE ?", search]}).then(function(quizes) {
      res.render('quizes/index.ejs', { quizes: quizes});
    }
   ).catch(function(error) { next(error);});
