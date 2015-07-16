28645f94c871f84eb6c00f0043f74fca92f6be7c
Luis <luis_cano@telefonica.net>
2015-07-16 21:53:28 +0200

    version 6.1, Autenticacion usuarios

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
+var sessionController = require('../controllers/session_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -17,6 +18,11 @@ router.param('quizId', quizController.load);
 
 //router.get('/quizes/question', quizController.question);
 //router.get('/quizes/answer',   quizController.answer);
+
+// Definicion de rutas de /session
+router.get('/login',   sessionController.new);
+router.post('/login',  sessionController.create);
+router.get('/logout',  sessionController.destroy);
 
 // Definicion de rutas de /quizies
 router.get('/quizes',                      quizController.index);
