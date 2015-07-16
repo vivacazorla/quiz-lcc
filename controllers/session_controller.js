41894bfd05b5698e484eb18807b17a74205fca10
Luis <luis_cano@telefonica.net>
2015-07-16 22:17:48 +0200

    version 6.2, Autorizaciones

--- controllers/session_controller.js
+++ controllers/session_controller.js
@@ -1,3 +1,12 @@
+// MW de autorizacion
+exports.loginRequired = function(req, res, next) {
+    if (req.session.user) {
+      next();
+    } else {
+      res.redirect('/login');
+    }
+};
+
 // GET /login
 exports.new = function(req, res) {
     var errors = req.session.errors || {};
